c9b771bc7e1cc9ae3b7090109d094fdcab56403d
Matteo <zmatteo.29@gmail.com>
2021-10-23 13:19:24 +0200

    Ex 1,2,3 sheet 1

---
+++ sheet1/code/ex3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Single precision
+    float eps = 0.5;
+    float old_eps;
+    while((1+eps) > 1){
+        old_eps = eps;
+        eps /= 2.;
+    }
+    cout << "Single precision machine epsilon: " << old_eps << endl;
+
+    // Double precision
+    double eps2 = 0.5;
+    double old_eps2;
+    while((1+eps2) > 1){
+        old_eps2 = eps2;
+        eps2 /= 2.;
+    }
+    cout << "Double precision machine epsilon: " << old_eps2 << endl;
+
+    // Long double precision
+    long double eps3 = 0.5;
+    long double old_eps3;
+    while((1+eps3) > 1){
+        old_eps3 = eps3;
+        eps3 /= 2.;
+    }
+    cout << "Long double precision machine epsilon: " << old_eps3 << endl;
+    
+    return 0;
+}
