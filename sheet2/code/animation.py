b70f1490eb3d76cde62487ee1f21734d74769c7e
Matteo <zmatteo.29@gmail.com>
2021-11-02 19:29:45 +0100

    done sheet2

--- sheet2/code/animation.py
+++ sheet2/code/animation.py
@@ -1,9 +1,11 @@
 from matplotlib import pyplot as plt 
 import pandas as pd
 from matplotlib.animation import FuncAnimation
+from matplotlib import animation
 
 global data 
 data = pd.read_csv("animation.csv")
+data2 = pd.read_csv("animation_newIC.csv")
 
 Ltot = 5
 xmin = -Ltot-0.5
@@ -20,23 +22,30 @@ axes.set_ylim(ymin, ymax)
 axes.set_axis_off()
 pendulum1, = ax.plot([0, 2], [0, 2], color='black', linewidth=3)
 pendulum2, = ax.plot([0, 2], [0, 2], color='black', linewidth=3)
-balls, = ax.plot([0, 0], [0, 0], 'o', markersize=15, color='black')
+pendulum3, = ax.plot([0, 2], [0, 2], color='gray', linewidth=3)
+pendulum4, = ax.plot([0, 2], [0, 2], color='gray', linewidth=3)
+balls, = ax.plot([0, 0, 0, 0], [0, 0, 0, 0], 'o', markersize=15, color='black')
 
 def init():
     pendulum1.set_data([], [])
     pendulum2.set_data([], [])
+    pendulum3.set_data([], [])
+    pendulum4.set_data([], [])
     balls.set_data([], [])
-    return pendulum1, pendulum2, balls
+    return pendulum1, pendulum2, pendulum3, pendulum4, balls
 def animate(i):
     pendulum1.set_data([0, data['x1'].to_numpy()[i]], [0, data['y1'].to_numpy()[i]])
     pendulum2.set_data([data['x1'].to_numpy()[i], data['x2'].to_numpy()[i]], [data['y1'].to_numpy()[i], data['y2'].to_numpy()[i]])
-    balls.set_data([data['x1'].to_numpy()[i], data['x2'].to_numpy()[i]], [data['y1'].to_numpy()[i], data['y2'].to_numpy()[i]])
-    return pendulum1, pendulum2, balls
+    pendulum3.set_data([0, data2['x1'].to_numpy()[i]], [0, data2['y1'].to_numpy()[i]])
+    pendulum4.set_data([data2['x1'].to_numpy()[i], data2['x2'].to_numpy()[i]], [data2['y1'].to_numpy()[i], data2['y2'].to_numpy()[i]])
+    balls.set_data([data['x1'].to_numpy()[i], data['x2'].to_numpy()[i], data2['x1'].to_numpy()[i], data2['x2'].to_numpy()[i]], [data['y1'].to_numpy()[i], data['y2'].to_numpy()[i], data2['y1'].to_numpy()[i], data2['y2'].to_numpy()[i]])
+    return pendulum1, pendulum2, pendulum3, pendulum4, balls
 
 anim = FuncAnimation(fig, animate, init_func=init, blit=True, interval=20)
 plt.show()
-
-data = pd.read_csv("animation.csv")
+writervideo = animation.FFMpegWriter(fps=60)
+anim.save('increasingStraightLine.mp4', writer=writervideo)
+plt.close()
 
 '''
 plt.plot(data['x1'], data['y1'])
--- sheet2/code/plot_e.py
+++ sheet2/code/plot_e.py
@@ -5,14 +5,21 @@ import numpy as np
 data = pd.read_csv("data.csv")
 data_newIC = pd.read_csv("data_newIC.csv")
 
+data['phi1'] = data['phi1'] * (180/np.pi)
+data['phi2'] = data['phi2'] * (180/np.pi)
+data_newIC['phi1'] = data_newIC['phi1'] * (180/np.pi)
+data_newIC['phi2'] = data_newIC['phi2'] * (180/np.pi)
 
-
+plt.plot(data['phi1'][0], data['phi2'][0], '.', markersize=16, color='red')
+plt.plot(data_newIC['phi1'][0], data_newIC['phi2'][0], '.', markersize=16, color='blue')
 plt.plot(data['phi1'], data['phi2'],'k-', label=r'IC: $\phi_1 = 50°$ & $\phi_2 = -120°$')
 plt.plot(data_newIC['phi1'], data_newIC['phi2'],'y-', label=r'IC: $\phi_1 = 51°$ & $\phi_2 = -120°$')
 plt.legend()
 plt.xlabel(r'$\phi_1$ (°)')
 plt.ylabel(r'$\phi_2$ (°)')
 plt.show()
+plt.plot(data['phi1dot'][0], data['phi2dot'][0], '.', markersize=16, color='red')
+plt.plot(data_newIC['phi1dot'][0], data_newIC['phi2dot'][0], '.', markersize=16, color='blue')
 plt.plot(data['phi1dot'], data['phi2dot'],'k-', label=r'IC: $\phi_1 = 50°$ & $\phi_2 = -120°$')
 plt.plot(data_newIC['phi1dot'], data_newIC['phi2dot'],'y-', label=r'IC: $\phi_1 = 51°$ & $\phi_2 = -120°$')
 plt.legend()
