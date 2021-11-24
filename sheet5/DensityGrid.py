72b8f4cfcd0af5478cc8d1020b71ebe1786ed726
Matteo <zmatteo.29@gmail.com>
2021-11-24 22:55:53 +0100

    done sheet5

--- sheet5/DensityGrid.py
+++ sheet5/DensityGrid.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt 
 import numpy as np
+import seaborn as sb
 
 ############################# Grid class ###########################
-class Grid:
+class DensityGrid:
     def __init__(self, H, N, m=1):
         self.H = H
         self.N = N
@@ -14,47 +15,53 @@ class Grid:
         self.W2 = np.zeros((N, N)) # weights matrix second order
 
     class Particle:
-        def __init__(self, r, grid_instance):
-            self.grid = grid_instance
+        def __init__(self, r, grid_instance, mass=1):
+            selfdensity_grid = grid_instance
             self.x = r[0] # x coord
             self.y = r[1] # y coord
             self.r = r # position vector
-            self.k, self.l = self.grid.find_kl(self.r) # get indices of the closest cell
-            self.xh = self.x/self.grid.h # x coordinate in units of h
-            self.yh = self.y/self.grid.h # y coordinate in units of h
+            self.k, self.l = selfdensity_grid.find_kl(self.r) # get indices of the closest cell
+            self.xh = self.x/selfdensity_grid.h # x coordinate in units of h
+            self.yh = self.y/selfdensity_grid.h # y coordinate in units of h
             self.rg = [self.xh, self.yh] # r in units of h
+            self.m = mass
 
     # Adds a particle to the grid at position r
     def add_particle(self, r):
         self.particles.append(self.Particle(r, self))
         k = self.particles[-1].k
         l = self.particles[-1].l
+        m = self.particles[-1].m
         # Update weights for zeroth order
-        self.W0[k,l] += 1
+        self.W0[k,l] += m
         # Update weights for the first order
         epsx, epsy = self.get_eps(self.particles[-1])
-        self.W1[k,l] += epsx * epsy
-        self.W1[k,l+1] += epsx * (1-self.top_hat(epsy))*(1-epsy)
-        self.W1[k,l-1] += epsx * self.top_hat(epsy)*(1-epsy)
-        self.W1[k+1,l] += (1-self.top_hat(epsx))*(1-epsx) * epsy
-        self.W1[k+1,l+1] += (1-self.top_hat(epsx))*(1-epsx) * (1-self.top_hat(epsy))*(1-epsy)
-        self.W1[k+1,l-1] += (1-self.top_hat(epsx))*(1-epsx) * self.top_hat(epsy)*(1-epsy)
-        self.W1[k-1,l] += self.top_hat(epsx)*(1-epsx) * epsy
-        self.W1[k-1,l+1] += self.top_hat(epsx)*(1-epsx) * (1-self.top_hat(epsy))*(1-epsy)
-        self.W1[k-1,l-1] += self.top_hat(epsx)*(1-epsx) * self.top_hat(epsy)*(1-epsy)
+        self.W1[k,l] += m*epsx * epsy
+        self.W1[k,l+1] += m*epsx * (1-self.top_hat(epsy))*(1-epsy)
+        self.W1[k,l-1] += m*epsx * self.top_hat(epsy)*(1-epsy)
+        self.W1[k+1,l] += m*(1-self.top_hat(epsx))*(1-epsx) * epsy
+        self.W1[k+1,l+1] += m*(1-self.top_hat(epsx))*(1-epsx) * (1-self.top_hat(epsy))*(1-epsy)
+        self.W1[k+1,l-1] += m*(1-self.top_hat(epsx))*(1-epsx) * self.top_hat(epsy)*(1-epsy)
+        self.W1[k-1,l] += m*self.top_hat(epsx)*(1-epsx) * epsy
+        self.W1[k-1,l+1] += m*self.top_hat(epsx)*(1-epsx) * (1-self.top_hat(epsy))*(1-epsy)
+        self.W1[k-1,l-1] += m*self.top_hat(epsx)*(1-epsx) * self.top_hat(epsy)*(1-epsy)
         # Update weights for the second order
-        epsx0, epsy0 = self.get_eps(self.particles[-1], [self.particles[-1].k-1, self.particles[-1].l-1]) 
-        epsx1, epsy1 = self.get_eps(self.particles[-1])
-        epsx2, epsy2 = self.get_eps(self.particles[-1], [self.particles[-1].k+1, self.particles[-1].l+1]) 
-        self.W2[k,l] += self.f(epsx1)*self.f(epsy1)
-        self.W2[k,l+1] += self.f(epsx1)*self.f(epsy2)
-        self.W2[k,l-1] += self.f(epsx1)*self.f(epsy0)
-        self.W2[k+1,l] += self.f(epsx2)*self.f(epsy1)
-        self.W2[k+1,l+1] += self.f(epsx2)*self.f(epsy2)
-        self.W2[k+1,l-1] += self.f(epsx2)*self.f(epsy0)
-        self.W2[k-1,l] += self.f(epsx0)*self.f(epsy1)
-        self.W2[k-1,l+1] += self.f(epsx0)*self.f(epsy2)
-        self.W2[k-1,l-1] += self.f(epsx0)*self.f(epsy0)
+        d1x = epsx - 1/2 # distance from neirest cell
+        d2x = 3/2 - epsx # distance from next cell
+        d0x = 1/2 + epsx # distance from previous cell
+        d1y = epsy - 1/2 
+        d2y = 3/2 - epsy
+        d0y = 1/2 + epsy
+        self.W2[k,l] += m*self.w2_func(d1x)*self.w2_func(d1y)
+        self.W2[k,l+1] += m*self.w2_func(d1x)*self.w2_func(d2y)
+        self.W2[k,l-1] += m*self.w2_func(d1x)*self.w2_func(d0y)
+        self.W2[k+1,l] += m*self.w2_func(d2x)*self.w2_func(d1y)
+        self.W2[k+1,l+1] += m*self.w2_func(d2x)*self.w2_func(d2y)
+        self.W2[k+1,l-1] += m*self.w2_func(d2x)*self.w2_func(d0y)
+        self.W2[k-1,l] += m*self.w2_func(d0x)*self.w2_func(d1y)
+        self.W2[k-1,l+1] += m*self.w2_func(d0x)*self.w2_func(d2y)
+        self.W2[k-1,l-1] += m*self.w2_func(d0x)*self.w2_func(d0y)
+
     # Get indices of the cell closest to position r
     def find_kl(self, r):
         k = int((self.H+r[0])/self.h)
@@ -67,11 +74,14 @@ class Grid:
             plt.fill_between([part.k-self.N/2, part.k-self.N/2+1], part.l-self.N/2, part.l-self.N/2+1, alpha=0.7, color=color) # complicated to explain this line, but just try it on paper to see it's correct
             plt.scatter(part.xh, part.yh, color='black')    
         edges = np.linspace(-self.N/2, self.N/2, self.N+1)
-        plt.xticks(edges)
-        plt.yticks(edges)
+        if len(self.particles) < 20:
+            plt.xticks(edges, labels=edges)
+            plt.yticks(edges, labels=edges)
         plt.grid()
         plt.xlim([-self.N/2, self.N/2])
         plt.ylim([-self.N/2, self.N/2])
+        plt.xlabel("x/h")
+        plt.ylabel("y/h")
             
     # Given a particle calculates distance (in units of h) from lower left corner of cell c=(k,l) and the particle position
     # if c is not provided it uses the closest cell to the particle
@@ -88,11 +98,12 @@ class Grid:
         else:
             return 1
     
-    def f(self, x):
-        if abs(x) < 0.5:
-            return 3/4 - x**2
-        elif abs(x) >= 0.5 and abs(x) < 1.5:
+    # pass the position and the index of the closest cell
+    def w2_func(self, x):
+        if abs(x) > 3/2:
+            return 0
+        elif abs(x) < 3/2 and abs(x) > 1/2:
             return 1/2*(3/2-abs(x))**2
         else:
-            return 0
+            return (3/4-x**2)
     
