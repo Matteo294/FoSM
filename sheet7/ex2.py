ex2 runs, ex3 sth wrong in the normalization? metropolis code does not work yet

sheet7/ex2.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from time import process_time
 
 def func(x):
     
-    d = len(x)
+    d = np.shape(x)[-1]
     f = 1
     for i in range(d):
         f *= 3/2*(1-x[i]**2)
@@ -11,10 +12,15 @@ def func(x):
 
 def midpoint(f,d,n):
 
+    m = np.arange(1/(2*n),1+1/(2*n),1/n)
+    M = np.array(np.meshgrid(*(m,)*d)).T.reshape(-1,d)
+    D = np.shape(M)[0]
+    fM = np.zeros(D)
+    for i in range(D):
+        fM[i] = f(M[i])
     # I = sum_i^n**2 1/n**2 f(m_i) where m_i is midpoint in each subcube
-    # midpoint integral, n subdivisions, d dimensions
-    # create lists of x and y --> midpoints, evaluate f at each midpoint and sum up, np.sum matrix?
-    pass
+    I = 1/n**2*np.sum(fM)
+    return I
 
 def monte_carlo_int(f,d,N):
 
@@ -27,6 +33,7 @@ def monte_carlo_int(f,d,N):
     I /= N
     return I
 
+
 def ex2():
     
     n = 6
@@ -37,4 +44,37 @@ def ex2():
     I_mpoint = []
     I_mc = []
 
-    pass
+    for i in d:
+        start = process_time()
+        Imp = midpoint(func,i,n)
+        stop = process_time()
+        t_mpoint.append(stop-start)
+        I_mpoint.append(Imp)
+
+        start = process_time()
+        Imc = monte_carlo_int(func,i,N)
+        stop = process_time()
+        t_mc.append(stop-start)
+        I_mc.append(Imc)
+
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('\n Exercise 2\n')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('\n Midpoint method:\n')
+    print(f'I for d in {1,2,...,10}: \n')
+    print(f'I = {I_mpoint}')
+    print('\n CPU times for each dimension: \n')
+    print(f't = {t_mpoint}')
+
+    print('\n Monte Carlo method:\n')
+    print(f'I for d in {1,2,...,10}: \n')
+    print(f'I = {I_mc}')
+    print('\n CPU times for each dimension: \n')
+    print(f't = {t_mc}')
+
+if __name__ == '__main__':
+    ex2()
+
+
+
+        

sheet7/ex3.py
@@ -1,6 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def metropolis_mc():
+def pnew(x):# normiert auf [1,20]
+    return 20/19*1/x**2
 
+def exact_inversion(x):
+    return 1/(1 - 19/20*x)
+
+
+def metropolis_mc(p,n,sigma=0.1):
+
+    xstart = float(np.random.uniform(1,20,1))
+    #xstart = 10
+    x = []
+    x.append(xstart)
+    for i in range(n-1):
+        p_i = p(xstart)
+        x_ii = float(np.random.normal(x[-1],sigma))
+        while x_ii < 1 or x_ii > 20 :
+            x_ii = abs(float(np.random.normal(x[-1],sigma)))
+        p_ii = p(x_ii)
+        #print(p_ii/p_i)
+        r = min(1,p_ii/p_i)
+        print(r)
+        u = np.random.uniform(0,1,1)
+        if u <= r:
+            x.append(x_ii)
+        else:
+            x.append(x[-1])
+
+    return x
+
+def ex3():
+
+    x = metropolis_mc(pnew,int(1e6),sigma=0.3)
+
+    #print(x)
+    print(max(x))
+
+    fig = plt.figure()
+    bins = 100
+    X = np.random.uniform(0,1,int(1e6))
+    Y = exact_inversion(X)
+    #plt.hist(Y,bins,density=True,color='r',alpha=0.5,label='exact inversion')
+    plt.hist(x,bins,color='k',label='Metropolis Monte Carlo')
+    plt.legend() 
+    plt.show()
+
+    
     pass
+
+
+if __name__ == '__main__':
+    ex3()
+
+    