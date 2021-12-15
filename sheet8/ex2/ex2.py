dabf769934e9477c8f2bb0d3db798bd80f6bf901
Matteo <zmatteo.29@gmail.com>
2021-12-15 15:10:37 +0100

    5 cyclces

--- sheet8/ex2/ex2.py
+++ sheet8/ex2/ex2.py
@@ -10,7 +10,8 @@ D = 1
 h = L/N1
 eps = 1
 nrelax = 5
-ncycles = 1
+phi0 = 1
+ncycles = 5
 
 def Jacobi_Iteration(x, Dinv, L, U, b):
     #print(x.shape, Dinv.shape, L.shape, U.shape, b.shape)
@@ -35,7 +36,7 @@ def create_restr_mat(N):
 def restrict_prolong_matrix(A, M1, M2):
     return np.einsum("ij,jk,kl->il", M1, A, M2)
 # Returns M*v
-def restrict_prolong_vector(M, v):
+def mat_vec_prod(M, v):
     return np.einsum("ij,j", M, v)
 
 # Returns D^-1, L, U given matrix A
@@ -51,68 +52,51 @@ def Dinv_L_U_decomposition(A):
                 U[i,j] = -A[i,j]
             elif i == j:
                 D_inv[i] = 1/A[i,i]
-    return D_inv, L, U
+    return D_inv, L, U 
 
-grid1 = np.linspace(0, L, N1)
-grid2 = np.linspace(0, L, N2)
-grid3 = np.linspace(0, L, N3)
-grid4 = np.linspace(0, L, N4)
+def solve_Vcycle(x, A, b):
+    if len(x) == 2:
+         # Decompose in to D L U
+        D_inv, L, U = Dinv_L_U_decomposition(A)
+        # One relaxation step 
+        x = Jacobi_Iteration(x, D_inv, L, U, b)
+    else:
+        # Decompose in to D L U
+        D_inv, L, U = Dinv_L_U_decomposition(A)
+        # One relaxation step 
+        x = Jacobi_Iteration(x, D_inv, L, U, b)
+        # Compute residual
+        residual = b - mat_vec_prod(A, x)
+        # Create restriction and prolongation matrices
+        R = create_restr_mat(len(residual))
+        P = 2*np.transpose(R)
+        # Restrict things
+        residual_r = mat_vec_prod(R, residual)
+        mat_vec_prod(R, b)
+        A_r = restrict_prolong_matrix(A, R, P)   
+        # Solver A*err = res
+        err = np.zeros(len(residual_r))
+        err = solve_Vcycle(err, A_r, residual_r)
+        # Prolong error and correct
+        err_p = mat_vec_prod(P, err)
+        x = x + err_p
+        # Further relaxation
+        x = Jacobi_Iteration(x, D_inv, L, U, b)
+    return x
 
-R1 = create_restr_mat(N1)
-R2 = create_restr_mat(N2)
-R3 = create_restr_mat(N3)
-P1 = 2*np.transpose(R1)
-P2 = 2*np.transpose(R2)
-P3 = 2*np.transpose(R3)
-
-A1 = np.zeros((N1, N1))
+A = np.zeros((N1, N1))
 for i in range(1, N1-1):
-    A1[i,i-1] = 1
-    A1[i,i] = -2
-    A1[i,i+1] = 1
-A1[0][0] = 1
-A1[N1-1][N1-1] = 1
+    A[i,i-1] = 1
+    A[i,i] = -2
+    A[i,i+1] = 1
+A[0][0] = 1
+A[N1-1][N1-1] = 1
+b = np.full(N1, -h**2/D/eps)
+b[0] = phi0
+b[N1-1] = phi0
+x = np.zeros(N1)
 
-A2 = restrict_prolong_matrix(A1, R1, P1)
-A3 = restrict_prolong_matrix(A2, R2, P2)
-A4 = restrict_prolong_matrix(A3, R3, P3)
-
-b1 = np.full(N1, -h**2/D/eps)
-b1[0] = 1
-b1[N1-1] = 1
-b2 = restrict_prolong_vector(R1, b1)
-b3 = restrict_prolong_vector(R2, b2)
-b4 = restrict_prolong_vector(R3, b3)
-
-D_inv1, L1, U1 = Dinv_L_U_decomposition(A1)
-D_inv2, L2, U2 = Dinv_L_U_decomposition(A2)
-D_inv3, L3, U3 = Dinv_L_U_decomposition(A3)
-D_inv4, L4, U4 = Dinv_L_U_decomposition(A4)
-
-x1 = np.zeros(N1)
-
-for n in range(ncycles):
-    print("Start v-cycle", n+1, ", Nrelax:", nrelax, ", Ncycles:", ncycles)
-    for _ in range(nrelax):
-        x1 = Jacobi_Iteration(x1, D_inv1, L1, U1, b1)
-    x2 = restrict_prolong_vector(R1, x1)
-    for _ in range(nrelax):
-        x2 = Jacobi_Iteration(x2, D_inv2, L2, U2, b2)
-    x3 = restrict_prolong_vector(R2, x2)
-    for _ in range(nrelax):
-        x3 = Jacobi_Iteration(x3, D_inv3, L3, U3, b3)
-    x4 = restrict_prolong_vector(R3, x3)
-    for _ in range(nrelax):
-        x4 = Jacobi_Iteration(x4, D_inv4, L4, U4, b4)
-    x3 = restrict_prolong_vector(P3, x4)
-    for _ in range(nrelax):
-        x3 = Jacobi_Iteration(x3, D_inv3, L3, U3, b3)
-    x2 = restrict_prolong_vector(P2, x3)
-    for _ in range(nrelax):
-        x2 = Jacobi_Iteration(x2, D_inv2, L2, U2, b2)
-    x1 = restrict_prolong_vector(P1, x2)
-    for _ in range(nrelax):
-        x1 = Jacobi_Iteration(x1, D_inv1, L1, U1, b1)
-    print("Mean squared error", sum(np.einsum("ij,j", A1, x1) - b1)**2)
-plt.plot(np.arange(0, L, h), x1)
-plt.show()
+print("Starting v-cycle")
+for _ in range(ncycles):
+    x = solve_Vcycle(x, A, b)
+    print("Residuals:", b - mat_vec_prod(A, x))
